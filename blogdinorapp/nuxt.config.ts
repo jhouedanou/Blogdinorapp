7a3bb989b7a7d134c1ee0216d37d71eb4d345863
Jean Luc Houedanou <jeanluc@houedanou.com>
2025-03-25 16:26:59 +0000

    lm

--- blogdinorapp/nuxt.config.ts
+++ blogdinorapp/nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   devtools: { enabled: process.env.NODE_ENV !== 'production' },
   modules: [
     '@nuxt/image',
