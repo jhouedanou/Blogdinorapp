8d11a6e16b258b1b4a42ffd0c6fe681cf9ea3085
Jean Luc Houedanou <jeanluc@houedanou.com>
2025-04-10 14:30:28 +0000

    Ajout du fichier de configuration nuxt.config.ts avec les paramètres de base pour le projet

--- blogdinorapp/nuxt.config.ts
+++ blogdinorapp/nuxt.config.ts
@@ -1,4 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   devtools: { enabled: process.env.NODE_ENV !== 'production' },
 
